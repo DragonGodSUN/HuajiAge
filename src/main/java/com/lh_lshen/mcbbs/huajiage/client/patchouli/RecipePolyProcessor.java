7df4e3ee0bbb1ddc27b6d1bbdadeef97d1b6531c
DragonGodSUN <642038132@qq.com>
2019-11-16 00:15:09 +0800

    初步完成了JO级手册

---
+++ src/main/java/com/lh_lshen/mcbbs/huajiage/client/patchouli/RecipePolyProcessor.java
@@ -0,0 +1,56 @@
+package com.lh_lshen.mcbbs.huajiage.client.patchouli;
+
+import com.lh_lshen.mcbbs.huajiage.recipelist.HuajiPolyRecipeList;
+import com.lh_lshen.mcbbs.huajiage.recipelist.HuajiRecipeList;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.CraftingManager;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.ResourceLocation;
+import vazkii.patchouli.api.IComponentProcessor;
+import vazkii.patchouli.api.IVariableProvider;
+import vazkii.patchouli.common.util.ItemStackUtil;
+
+public class RecipePolyProcessor implements IComponentProcessor {
+
+	ItemStack inItem;
+	ItemStack outItem;
+	
+	@Override
+	public void setup(IVariableProvider<String> variables) {
+		String material = variables.get("recipe");
+		inItem = new ItemStack(Item.getByNameOrId(material));
+		outItem = HuajiPolyRecipeList.instance().getSmeltingResult(inItem);
+	}
+
+	@Override
+	public String process(String key) {
+
+		if(key.equals("item_in")) {
+
+			ItemStack stack = outItem.isEmpty() ? ItemStack.EMPTY : inItem;
+			
+			return ItemStackUtil.serializeStack(stack);
+		}
+		else if(key.equals("item_out")) {
+
+			ItemStack stack = outItem.isEmpty() ? ItemStack.EMPTY : outItem;
+			
+			return ItemStackUtil.serializeStack(stack);
+		}
+		
+		else if(key.equals("icount"))
+			return Integer.toString(outItem.getCount());
+		
+		else if(key.equals("ipool"))
+			return Integer.toString(HuajiPolyRecipeList.instance().getPoint(inItem));
+		
+		else if(key.equals("iname"))
+			return "$(bold)"+inItem.getDisplayName();
+		
+		return null;
+	}
+
+}
